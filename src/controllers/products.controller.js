1d0ca9688674e52bc463ea498d105ae9c3aaf9ca
MolinaMS95 <matheusmolina95@gmail.com>
2022-11-26 14:01:31 -0300

    fix: add (req,res) arguments to getProducts function

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,6 @@
 import { productsCollection } from "../database/db.js";
 
-export async function getProducts() {
+export async function getProducts(req, res) {
   try {
     const products = await productsCollection.find({}).toArray();
     res.send(products);
