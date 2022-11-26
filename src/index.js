10a18b44afd5bdef0b796865b6fc5d218ffa2250
MolinaMS95 <matheusmolina95@gmail.com>
2022-11-26 13:56:08 -0300

    fix: add use product.routes on index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import usersRoutes from "./routes/users.routes.js";
+import productsRoutes from "./routes/products.routes.js"
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(usersRoutes);
+app.use(productsRoutes);
 
 const port = process.env.PORT || 3333;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
