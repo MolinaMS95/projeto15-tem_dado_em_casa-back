c9dcdd6f3192b043889fd6af5a13f79b25b84b83
MolinaMS95 <matheusmolina95@gmail.com>
2022-11-26 20:45:16 -0300

    feat: get cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { authValidation } from "../middlewares/authValidation.middleware";
 
 const router = Router();
+router.use(authValidation);
 
 router.get("/cart", getCart);
 router.post("/cart", insertProduct);
-router.delete("/cart", removeProduct)
+router.delete("/cart", removeProduct);
 
 export default router;
