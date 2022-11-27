11c1da49fe2581456895574dcc6196a8deab4d63
MolinaMS95 <matheusmolina95@gmail.com>
2022-11-27 17:49:52 -0300

    fix: add game name to cart schema

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -4,4 +4,5 @@ export const cartSchema = joi.object({
   userId: joi.object().required(),
   gameId: joi.string().required(),
   price: joi.number().required(),
+  gameName: joi.string().required()
 });
