8d8cc7ba1a70692e970d90d9866f7bf4c04b9137
MolinaMS95 <matheusmolina95@gmail.com>
2022-11-28 15:52:22 -0300

    feat: clear cart after checkout

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
 import { cartSchemaValidation } from "../middlewares/cartSchemaValidation.middleware.js";
 import { gameExistsValidation } from "../middlewares/gameExistsValidation.middleware.js";
-import { getCart, insertProduct, removeProduct } from "../controllers/cart.controller.js";
+import { getCart, insertProduct, removeProduct, clearCart } from "../controllers/cart.controller.js";
 
 const router = Router();
 router.use(authValidation);
@@ -10,5 +10,6 @@ router.use(authValidation);
 router.get("/cart", getCart);
 router.post("/cart", cartSchemaValidation, insertProduct);
 router.delete("/cart/:id", gameExistsValidation, removeProduct);
+router.delete("/cart", clearCart);
 
 export default router;
